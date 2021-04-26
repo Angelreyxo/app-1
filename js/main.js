f645c755d791f329ccff8b8b0487be4ca8a0e4b2
Angel Reynoso <angelreynosoduarte@gmail.com>
2021-04-26 14:44:17 -0400

    TT

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// addEventlistener('DOMContentLoaded', ()=>{
+//     const btn_menu = document.querySelector('.btn_menu');
+//     if(btn_menu){
+//         btn_menu.addEventListener('click', ()=>{
+//             const btn_menu = document.querySelector('.menu_items');
+//             btn_menu.classList.toggle('show')
+//         })
+//     }
+
+// } )
+
+function showmenu() {
+
+    document.getElementById("otroModo").classList.toggle("show");
+  
+  }
